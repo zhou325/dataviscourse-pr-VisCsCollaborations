f2ca69f3dc36c0c0e19dacd84b6c23913ed74ffb
Youjia <u1208920@utah.edu>
2018-11-08 10:29:07 -0700

    add some new scripts, and modify the html file and script.js

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -1,14 +1,14 @@
 console.log("i am here")
 
-d3.json('dataProcessing/data/confArticles.json').then(articleData => {
+d3.json('data/confArticles.json').then(articleData => {
     console.log(articleData);
 });
 
-d3.json('dataProcessing/data/collaborations.json').then(collData => {
+d3.json('data/collaborations.json').then(collData => {
     console.log(collData);
 });
 
-d3.json('dataProcessing/data/collaborationsDetails.json').then(collDeData => {
+d3.json('data/collaborationsDetails.json').then(collDeData => {
     console.log(collDeData);
 });
 
